b2301f1c5af259b63e9ef3ec290ccb4cd1624cb5
cchoudhari1@gmail.com <cchoudhari1@gmail.com>
2020-04-15 11:35:54 +0530

    [Chetan] Add. added xamarine user interface for employee management system

---
+++ Greeting/GreetingApp/GreetingApp/EditEmployee.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GreetingApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditEmployee : ContentPage
+    {
+        public EditEmployee()
+        {
+            InitializeComponent();
+        }
+        private void EmployeeEdit(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new EmployeeInfo());
+        }
+    }
+}
